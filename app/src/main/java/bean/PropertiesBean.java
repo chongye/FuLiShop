b2d8bea4c18d758973b1b36de0714557fc7a8548
YeChong <770678496@qq.com>
2016-10-13 20:31:56 +0800

    添加实体类

---
+++ app/src/main/java/bean/PropertiesBean.java
@@ -0,0 +1,103 @@
+package bean;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class PropertiesBean {
+    private int id;
+
+    private int goodsId;
+
+    private int colorId;
+
+    private String colorName;
+
+    private String colorCode;
+
+    private String colorImg;
+
+    private String colorUrl;
+
+    private Object albums ;
+
+    public PropertiesBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(int goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public int getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColorImg() {
+        return colorImg;
+    }
+
+    public void setColorImg(String colorImg) {
+        this.colorImg = colorImg;
+    }
+
+    public String getColorUrl() {
+        return colorUrl;
+    }
+
+    public void setColorUrl(String colorUrl) {
+        this.colorUrl = colorUrl;
+    }
+
+    public Object getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(Object albums) {
+        this.albums = albums;
+    }
+
+    @Override
+    public String toString() {
+        return "PropertiesBean{" +
+                "id=" + id +
+                ", goodsId=" + goodsId +
+                ", colorId=" + colorId +
+                ", colorName='" + colorName + '\'' +
+                ", colorCode='" + colorCode + '\'' +
+                ", colorImg='" + colorImg + '\'' +
+                ", colorUrl='" + colorUrl + '\'' +
+                ", albums=" + albums +
+                '}';
+    }
+}
