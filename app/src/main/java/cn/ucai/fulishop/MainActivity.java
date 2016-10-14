983d29ddc9ddf4087a5d0b9650b66e103cfa7df6
YeChong <770678496@qq.com>
2016-10-14 20:10:17 +0800

    底部菜单购物车的互斥，添加Fragment

--- app/src/main/java/cn/ucai/fulishop/MainActivity.java
+++ app/src/main/java/cn/ucai/fulishop/MainActivity.java
@@ -1,17 +1,109 @@
 package cn.ucai.fulishop;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import java.util.ArrayList;
+
+import bean.NewGoodsBean;
+import cn.ucai.fulishop.fragment.BoutiqueFragment;
+import cn.ucai.fulishop.fragment.CartsFragment;
+import cn.ucai.fulishop.fragment.CategoryFragment;
+import cn.ucai.fulishop.fragment.NewGoodsFragment;
+import cn.ucai.fulishop.fragment.PersonalFragment;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton mNewGoods,mBoutique,mCategory,mCart,mPersonal;
 
+    NewGoodsFragment newGoodsFragment;
+    BoutiqueFragment boutiqueFragment;
+    CategoryFragment categoryFragment;
+    CartsFragment cartsFragment;
+    PersonalFragment personalFragment;
+    ArrayList<Fragment> fragments;
+    FragmentTransaction fragmentTransaction;
+    int indext;
+    int currentIndext = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
-    public void onCheckedChange(View view){
 
+    private void initView() {
+        mNewGoods = (RadioButton) findViewById(R.id.rb_NewsGood);
+        mBoutique = (RadioButton) findViewById(R.id.rb_Boutique);
+        mCategory = (RadioButton) findViewById(R.id.rb_Category);
+        mCart = (RadioButton) findViewById(R.id.rb_Cart);
+        mPersonal = (RadioButton) findViewById(R.id.rb_Personal);
+
+        newGoodsFragment = new NewGoodsFragment();
+        boutiqueFragment = new BoutiqueFragment();
+        categoryFragment = new CategoryFragment();
+        cartsFragment = new CartsFragment();
+        personalFragment = new PersonalFragment();
+
+        fragments = new ArrayList<>();
+        fragments.add(newGoodsFragment);
+        fragments.add(boutiqueFragment);
+        fragments.add(categoryFragment);
+        fragments.add(cartsFragment);
+        fragments.add(personalFragment);
+
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container,newGoodsFragment).show(newGoodsFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void onCheckedChange(View view){
+        switch (view.getId()){
+            case R.id.rb_NewsGood:
+                indext = 0;
+                break;
+            case R.id.rb_Boutique:
+                indext = 1;
+                break;
+            case R.id.rb_Category:
+                indext = 2;
+                break;
+            case R.id.rb_Cart:
+                indext = 3;
+                break;
+            case R.id.rb_Personal:
+                indext = 4;
+                break;
+            default:
+                indext = 0;
+                break;
+        }
+        switchFragment(indext);
+        if(view.getId()!=R.id.rb_Cart){
+            mCart.setChecked(false);
+        }else{
+            mNewGoods.setChecked(false);
+            mBoutique.setChecked(false);
+            mCategory.setChecked(false);
+            mPersonal.setChecked(false);
+        }
+    }
+
+    private void switchFragment(int indext) {
+        if(indext == currentIndext){
+            return;
+        }
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fragment = fragments.get(indext);
+        if(!fragment.isAdded()){
+            ft.add(R.id.fragment_container,fragment);
+        }
+        ft.show(fragment).hide(fragments.get(currentIndext)).commit();
+        currentIndext = indext;
     }
 }
---
+++ app/src/main/java/cn/ucai/fulishop/utils/MFGT.java
@@ -0,0 +1,24 @@
+package cn.ucai.fulishop.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulishop.MainActivity;
+import cn.ucai.fulishop.R;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+}
