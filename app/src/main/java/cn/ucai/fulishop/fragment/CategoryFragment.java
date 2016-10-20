e39a28f4c2040349c24ad45ea1d555adfbf6c8b3
YeChong <770678496@qq.com>
2016-10-20 15:49:26 +0800

    分类首界面的fragment文件的添加，及Adapter

--- app/src/main/java/cn/ucai/fulishop/fragment/CategoryFragment.java
+++ app/src/main/java/cn/ucai/fulishop/fragment/CategoryFragment.java
@@ -1,19 +1,43 @@
 package cn.ucai.fulishop.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import bean.CategoryChildBean;
+import bean.CategoryGroupBean;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulishop.Dao.NetDao;
 import cn.ucai.fulishop.R;
+import cn.ucai.fulishop.adapter.CategoryAdapter;
+import cn.ucai.fulishop.utils.ConvertUtils;
+import cn.ucai.fulishop.utils.L;
+import cn.ucai.fulishop.utils.OkHttpUtils;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CategoryFragment extends Fragment {
+public class CategoryFragment extends BaseFragment {
+    CategoryAdapter mAdapter;
+    ArrayList<CategoryGroupBean> mGruopList;
+    ArrayList<ArrayList<CategoryChildBean>> mChildList;
 
+    Context mContext;
+
+    @BindView(R.id.Elv)
+    ExpandableListView Elv;
 
     public CategoryFragment() {
         // Required empty public constructor
@@ -24,7 +48,73 @@ public class CategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        ButterKnife.bind(this, view);
+        super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+
+    @Override
+    protected void initView() {
+        mContext = getContext();
+        mGruopList = new ArrayList<>();
+        mChildList = new ArrayList<>();
+        mAdapter = new CategoryAdapter(mContext,mGruopList,mChildList);
+        Elv.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void initData() {
+        NetDao.downLoadCategoryGroup(mContext, new OkHttpUtils.OnCompleteListener<CategoryGroupBean[]>() {
+            @Override
+            public void onSuccess(CategoryGroupBean[] result) {
+                /*ArrayList<CategoryGroupBean> groupList = ConvertUtils.array2List(result);
+                mGruopList.addAll(groupList);*/
+                mGruopList = ConvertUtils.array2List(result);
+                for (int i = 0; i < mGruopList.size(); i++) {
+                    mChildList.add(new ArrayList<CategoryChildBean>());
+                    getChildList(mGruopList.get(i).getId(), i);
+                    /*NetDao.downLoadCategoryChild(mContext, result[i].getId(), new OkHttpUtils.OnCompleteListener<CategoryChildBean[]>() {
+                        @Override
+                        public void onSuccess(CategoryChildBean[] result) {
+                            ArrayList<CategoryChildBean> list = ConvertUtils.array2List(result);
+                            mChildList.add(list);
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });*/
+                }
+            }
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    private void getChildList(int id, final int indext) {
+        NetDao.downLoadCategoryChild(mContext, id, new OkHttpUtils.OnCompleteListener<CategoryChildBean[]>() {
+            @Override
+            public void onSuccess(CategoryChildBean[] result) {
+                ArrayList<CategoryChildBean> list = ConvertUtils.array2List(result);
+                mChildList.set(indext,list);
+                L.e("Category","list="+mChildList.get(0).toString());
+                mAdapter.initData(mGruopList,mChildList);
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void setListener() {
+
     }
 
 }
