48896a204e14e770627272f090d9ea7c6490a72f
YeChong <770678496@qq.com>
2016-10-24 20:16:51 +0800

    SQLite三个工具类的添加

---
+++ app/src/main/java/cn/ucai/fulishop/Dao/UserAvatarDao.java
@@ -0,0 +1,42 @@
+package cn.ucai.fulishop.Dao;
+
+import android.content.Context;
+
+import bean.UserAvatar;
+
+/**
+ * Created by Administrator on 2016/10/24.
+ * String sql = "create table "+TB_USER+"("
+ +"id integer primary key autoincrement,"
+ +"muserName varchar,"
+ +"muserNick varchar,"
+ +"mavatarId integer,"
+ +"mavatarPath varchar,"
+ +"mavatarSuffix varchar,"
+ +"mavatarType integer,"
+ +"mavatarLastUpdateTime varchar)";
+ */
+
+public class UserAvatarDao {
+    public static final String USER_TABLE = "t_superweb_user";
+    public static final String USER_NAME = "m_user_name";
+    public static final String USER_NICK = "m_user_nick";
+    public static final String USER_AVATAR_ID = "m_avatar_id";
+    public static final String USER_AVATAR_PATH = "m_avatar_path";
+    public static final String USER_AVATAR_SUFFIX = "m_avatar_suffix";
+    public static final String USER_AVATAR_TYPE = "m_avatar_type";
+    public static final String USER_AVATAR_LASTUPDATETIME = "m_avatar_lastupdatetime";
+
+    public UserAvatarDao(Context context){
+        DBManager.getInstance().onInit(context);
+    }
+    public boolean saveUserAvatar(UserAvatar user){
+        return DBManager.getInstance().saveUserData(user);
+    }
+    public UserAvatar getUserAvatar(String username){
+        return DBManager.getInstance().getUser(username);
+    }
+    public boolean updateUserAvatar(UserAvatar user){
+        return false;
+    }
+}
