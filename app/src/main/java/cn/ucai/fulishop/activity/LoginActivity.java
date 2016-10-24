e65b6efe45701430c3c85f3e2d26dc43fda03b73
YeChong <770678496@qq.com>
2016-10-24 20:18:40 +0800

    在登录时将数据储存到SQLite数据库

--- app/src/main/java/cn/ucai/fulishop/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulishop/activity/LoginActivity.java
@@ -1,26 +1,37 @@
 package cn.ucai.fulishop.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
 import bean.Result;
+import bean.UserAvatar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulishop.Dao.NetDao;
+import cn.ucai.fulishop.Dao.UserAvatarDao;
+import cn.ucai.fulishop.FuLiShopApplication;
 import cn.ucai.fulishop.R;
-import cn.ucai.fulishop.fragment.PersonalFragment;
 import cn.ucai.fulishop.utils.CommonUtils;
 import cn.ucai.fulishop.utils.I;
+import cn.ucai.fulishop.utils.L;
 import cn.ucai.fulishop.utils.MFGT;
 import cn.ucai.fulishop.utils.OkHttpUtils;
 import cn.ucai.fulishop.utils.ResultUtils;
+import cn.ucai.fulishop.utils.SharePrefrenceUtils;
 import cn.ucai.fulishop.views.DisplayUtils;
 
 public class LoginActivity extends BaseActivity {
+    private final String TB_USER = "tb_user";
 
     @BindView(R.id.et_UserName)
     EditText etUserName;
@@ -28,6 +39,7 @@ public class LoginActivity extends BaseActivity {
     EditText etPassword;
     String userName;
     String password;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void initView() {
         etUserName.setText(userName);
+        mContext = this;
     }
 
     @Override
@@ -68,19 +81,38 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void setLogin() {
-        NetDao.login(this, userName, password, new OkHttpUtils.OnCompleteListener<Result>() {
+        NetDao.login(this, userName, password, new OkHttpUtils.OnCompleteListener<String>() {
             @Override
-            public void onSuccess(Result result) {
+            public void onSuccess(String result) {
+                Result result1 = ResultUtils.getResultFromJson(result, UserAvatar.class);
                 if(result == null){
                     return;
-                }else if(result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER){
+                }else if(result1.getRetCode() == I.MSG_LOGIN_UNKNOW_USER){
                     CommonUtils.showShortToast("账户不存在");
                     return;
-                }else if(result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD){
+                }else if(result1.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD){
                     CommonUtils.showShortToast("账户密码错误");
                     return;
                 }
-                CommonUtils.showShortToast("登录成功");
+                UserAvatar user = (UserAvatar) result1.getRetData();
+                L.e("user","user="+user.toString());
+
+                 /*//  result.getRetData()为json的String文件
+                 Gson gson = new Gson();
+                 String json = result.getRetData().toString();
+                 UserAvatar user = gson.fromJson(json,UserAvatar.class);*/
+                CommonUtils.showShortToast(user.toString());
+                UserAvatarDao userDao = new UserAvatarDao(mContext);
+                userDao.saveUserAvatar(user);
+                boolean isSuccess = userDao.saveUserAvatar(user);
+                if(isSuccess){
+                    SharePrefrenceUtils.getInstance(mContext).saveUser(user.getMuserName());
+                    FuLiShopApplication.setUser(user);
+                    setResult(I.REQUEST_CODE_REQUEST);
+                    MFGT.finish((Activity) mContext);
+                }else {
+                    CommonUtils.showShortToast(R.string.user_database_error);
+                }
             }
 
             @Override
@@ -88,6 +120,31 @@ public class LoginActivity extends BaseActivity {
 
             }
         });
+    }/*private String muserName;
+    private String muserNick;
+    private int mavatarId;
+    private String mavatarPath;
+    private String mavatarSuffix;
+    private int mavatarType;
+    private String mavatarLastUpdateTime;*/
+
+    private void onSaveSQLite(UserAvatar user) {
+        SQLiteDatabase sq = openOrCreateDatabase("user.db",MODE_PRIVATE,null);
+        String sql = "create table "+TB_USER+"("
+                +"id integer primary key autoincrement,"
+                +"muserName varchar,"
+                +"muserNick varchar,"
+                +"mavatarId integer,"
+                +"mavatarPath varchar,"
+                +"mavatarSuffix varchar,"
+                +"mavatarType integer,"
+                +"mavatarLastUpdateTime varchar)";
+        L.e("sql",sql);
+        sq.execSQL(sql);
+        sq.execSQL("insert into "+TB_USER+"(muserName,muserNick,mavatarId,mavatarPath,mavatarSuffix" +
+                ",mavatarType,mavatarLastUpdateTime) values('"+user.getMuserName()+"','"+user.getMuserNick()+"','"
+                +user.getMavatarId()+"','"+user.getMavatarPath()+"','"+user.getMavatarSuffix()+"','"+user.getMavatarType()
+                +"','"+user.getMavatarLastUpdateTime()+"')");
     }
     public void onBackPressed(){
         finish();
