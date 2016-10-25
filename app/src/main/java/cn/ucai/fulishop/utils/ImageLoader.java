6b46b18a3e58f8924b1bb1a372bc2ccb78055b0e
YeChong <770678496@qq.com>
2016-10-25 20:53:53 +0800

    添加上传图片时的工具，和选择相册时的工具

--- app/src/main/java/cn/ucai/fulishop/utils/ImageLoader.java
+++ app/src/main/java/cn/ucai/fulishop/utils/ImageLoader.java
@@ -359,9 +359,11 @@ public class ImageLoader {
                 .showImage(context);
     }
     /*http://101.251.196.90:8000/FuLiCenterServerV2.0/downloadAvatar?name_or_hxid=yechong&avatarType=user_avatar&m_avatar_suffix=.jpg&width=200&height=200*/
+   // http://101.251.196.90:8000/FuLiCenterServerV2.0/downloadAvatar?name_or_hxid=yechong&avatarType=user_avatar&m_avatar_suffix=.jpg&width=60&height=60
     public static String getUrl(UserAvatar user){
         String url = I.SERVER_ROOT+I.REQUEST_DOWNLOAD_AVATAR+I.QUESTION+I.NAME_OR_HXID+I.EQUAL+user.getMuserName()
-                +I.AND+I.AVATAR_TYPE+I.EQUAL+user.getMavatarType()+I.AND+I.AVATAR_SUFFIX+user.getMavatarSuffix()+I.AND+"width=200&height=200";
+                +I.AND+I.AVATAR_TYPE+I.EQUAL+"user_avatar"+I.AND+I.AVATAR_SUFFIX+I.EQUAL+user.getMavatarSuffix()+I.AND+"width=60&height=60";
+        L.e("Url","Url+"+url);
         return url;
 
     }
